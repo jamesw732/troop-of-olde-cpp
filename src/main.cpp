8a753016819ded6804f85bb3bd9d300a3c249783
jamesw732 <jamesmw732@gmail.com>
2025-09-03 22:50:19 -0400

    Integrate flecs, make movement fixed-timestep

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,21 @@
 #include "raylib.h"
 #include "flecs.h"
 #include <iostream>
-#include <vector>
 
-struct TransformComponent {
+const float MOVE_UPDATE_RATE = 1.0 / 20.0;
+
+struct Transformation {
     Vector3 pos;
     Vector3 rot;
     Vector3 scale;
 };
 
-void RenderSystem(std::vector<TransformComponent> iter, Camera3D camera) {
-    BeginDrawing();
-        ClearBackground(RAYWHITE);
-        BeginMode3D(camera);
-            // Draw each entity in the scene
-            for (TransformComponent comp: iter) {
-                DrawCube(comp.pos, 2.0f, 2.0f, 2.0f, RED);
-                DrawCubeWires(comp.pos, 2.0f, 2.0f, 2.0f, MAROON);
-            }
-            DrawGrid(10, 1.0f);
-        EndMode3D();
-        DrawText("Welcome to the third dimension!", 10, 40, 20, DARKGRAY);
-        DrawFPS(10, 10);
-    EndDrawing();
-}
-
-void MovementSystem(std::vector<TransformComponent> &iter) {
-    for (TransformComponent &comp: iter) {
-        if (IsKeyDown(KEY_W)) {
-            comp.pos.z -= 0.1;
-        }
-        if (IsKeyDown(KEY_S)) {
-            comp.pos.z += 0.1;
-        }
-        if (IsKeyDown(KEY_D)) {
-            comp.pos.x += 0.1;
-        }
-        if (IsKeyDown(KEY_A)) {
-            comp.pos.x -= 0.1;
-        }
-    }
-}
-
 int main(void)
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
+    SetTargetFPS(60);
     InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera mode");
-
-    flecs::world world;
 
     // Define the camera to look into our 3d world
     Camera3D camera = { 0 };
@@ -58,20 +25,59 @@ int main(void)
     camera.fovy = 45.0f;                                // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
 
-    TransformComponent transformComp = {
+    flecs::world world;
+
+    Transformation transformComp = {
          {0.0f, 0.0f, 0.0f},
          {0.0f, 0.0f, 0.0f},
          {1.0f, 1.0f, 1.0f}
     };
-    std::vector<TransformComponent> transformComps;
-    transformComps.push_back(transformComp);
 
-    SetTargetFPS(60);
+    auto e = world.entity("character");
+    e.add<Transformation>();
+    e.set<Transformation>(transformComp);
+    auto move_sys = world.system<Transformation>()
+        .interval(MOVE_UPDATE_RATE)
+        .each([](flecs::iter& it, size_t, Transformation& t) {
+                if (IsKeyDown(KEY_W)) {
+                    t.pos.z -= 0.25;
+                }
+                if (IsKeyDown(KEY_S)) {
+                    t.pos.z += 0.25;
+                }
+                if (IsKeyDown(KEY_D)) {
+                    t.pos.x += 0.25;
+                }
+                if (IsKeyDown(KEY_A)) {
+                    t.pos.x -= 0.25;
+                }
+            }
+        );
+    auto render_sys = world.system<Transformation>()
+        .each([&camera](flecs::iter& it, size_t, Transformation& t) {
+                BeginMode3D(camera);
+                    DrawCube(t.pos, 2.0f, 2.0f, 2.0f, RED);
+                    DrawCubeWires(t.pos, 2.0f, 2.0f, 2.0f, MAROON);
+                EndMode3D();
+            }
+        );
 
     while (!WindowShouldClose())
     {
-        MovementSystem(transformComps);
-        RenderSystem(transformComps, camera);
+        float dt = GetFrameTime();
+        world.progress(dt);
+        // Do rendering
+        BeginDrawing();
+            // Draw the UI
+            ClearBackground(RAYWHITE);
+            BeginMode3D(camera);
+                DrawGrid(10, 1.0f);
+            EndMode3D();
+            DrawText("Welcome to the third dimension!", 10, 40, 20, DARKGRAY);
+            DrawFPS(10, 10);
+            // Draw each entity in the scene
+            render_sys.run();
+        EndDrawing();
     }
     CloseWindow();
 
