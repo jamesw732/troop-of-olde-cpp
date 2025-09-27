f670fde85f5b3affeaa4ecf355b905533874fa0d
jamesw732 <jamesmw732@gmail.com>
2025-09-27 19:23:23 -0400

    flatten systems

--- src/client/systems/movement_systems.hpp
+++ src/client/systems/movement_systems.hpp
@@ -6,13 +6,13 @@
 #include "client/input_buffer.hpp"
 #include "client/input_handler.hpp"
 #include "client/components/player.hpp"
-#include "client/systems/movement_system.hpp"
-#include "client/systems/movement_networking_system.hpp"
 #include "shared/helpers/movement.hpp"
 #include "shared/components/movement.hpp"
 #include "shared/components/physics.hpp"
 #include "shared/components/ticks.hpp"
 #include "shared/const.hpp"
+#include "shared/serialize/helpers.hpp"
+#include "shared/serialize/serialize_movement.hpp"
 
 
 inline void register_movement_reconcile_system(flecs::world& world, InputBuffer& input_buffer) {
@@ -55,17 +55,42 @@ inline void register_movement_system(flecs::world& world) {
     auto move_sys = world.system<Position, MovementInput, LocalPlayer>()
         .interval(MOVE_UPDATE_RATE)
         .each([](Position& pos, MovementInput& input, LocalPlayer) {
-                movement_system(pos, input);
+                process_movement_input(pos, input);
             }
         );
 }
 
 inline void register_movement_networking_system(flecs::world& world, ENetPeer* peer, InputBuffer& input_buffer,
-        uint16_t& movement_tick) {
+        uint16_t& tick) {
     auto move_networking_sys = world.system()
         .interval(MOVE_UPDATE_RATE)
-        .each([peer, &input_buffer, &movement_tick]() {
-                movement_networking_system(peer, input_buffer, movement_tick);
+        .each([peer, &input_buffer, &tick]() {
+            // Construct movement input packet from input buffer and send to server
+            MovementInputPacket input_data;
+            input_data.tick = tick;
+            for (MovementInput input: input_buffer.buffer) {
+                // std::cout << (int) input.x << ", " << (int) input.z << std::endl;
+                input_data.inputs.push_back(input);
+            }
+            Buffer data_buffer;
+            size_t size = bitsery::quickSerialization(OutputAdapter{data_buffer}, input_data);
+            ENetPacket* packet = enet_packet_create(data_buffer.data(), size, 0);
+            enet_peer_send(peer, 0, packet);
+
+            // Test serialize
+            // InputPacket res;
+            // auto state = bitsery::quickDeserialization(
+            //     InputAdapter{
+            //         data_buffer.data(),
+            //         size
+            //     },
+            //     res
+            // );
+            // std::cout << "Sending inputs: ";
+            // for (MovementInput input: res.inputs) {
+            //     std::cout << (int) input.x << ", " << (int) input.z << " ";
+            // }
+            // std::cout << std::endl;
             }
         );
 }
--- src/client/systems/render_systems.hpp
+++ src/client/systems/render_systems.hpp
@@ -3,14 +3,16 @@
 #include "flecs.h"
 #include "raylib-cpp.hpp"
 
-#include "client/systems/render_system.hpp"
 #include "shared/components/physics.hpp"
 
 
 inline flecs::system register_render_system(flecs::world& world, raylib::Camera3D& camera) {
     return world.system<Position>("RenderSystem")
         .each([&camera](Position& pos) {
-                render_system(camera, pos);
+            BeginMode3D(camera);
+                DrawCube(pos.val, 2.0f, 2.0f, 2.0f, RED);
+                DrawCubeWires(pos.val, 2.0f, 2.0f, 2.0f, MAROON);
+            EndMode3D();
             }
         );
 }
