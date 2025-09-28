97782d29fc49638eb7929bb60953fee4cc1abeb9
jamesw732 <jamesmw732@gmail.com>
2025-09-27 20:47:01 -0400

    modularize client networking code

--- src/client.cpp
+++ src/client.cpp
@@ -14,6 +14,7 @@
 #include "client/fonts.hpp"
 #include "client/input_handler.hpp"
 #include "client/input_buffer.hpp"
+#include "client/network.hpp"
 #include "client/systems/movement_systems.hpp"
 #include "client/systems/render_systems.hpp"
 #include "shared/components/physics.hpp"
@@ -32,62 +33,25 @@ int main(void)
     const int screenHeight = 450;
     SetTargetFPS(60);
     InitWindow(screenWidth, screenHeight, "Troop of Olde");
-    raylib::Font cascadiaMono = raylib::LoadFont(cascadiaMonoPath);
 
     // Initialize camera attributes
     raylib::Camera3D camera;
-    // Camera3D camera = { 0 };
     camera.position = raylib::Vector3(0.0f, 10.0f, 10.0f);
     camera.target = raylib::Vector3(0.0f, 0.0f, 0.0f);      // Camera looking at point
     camera.up = raylib::Vector3(0.0f, 1.0f, 0.0f);
     camera.fovy = 45.0f;                                // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
 
-    // Initialize ENet client
-    if (enet_initialize() != 0) {
-        std::cout << "An error occurred while initializing ENet" << std::endl;
-        return EXIT_FAILURE;
-    }
+    raylib::Font cascadiaMono = raylib::LoadFont(cascadiaMonoPath);
 
-    ENetHost* client = {0};
-    client = enet_host_create(
-        NULL, // Create a client host
-        1, // Only allow 1 outgoing connection
-        2, // Allow up to 2 channels to be used, 0 and 1
-        0, // Assume any amount of incoming bandwidth
-        0 // Assume any amount of outgoing bandwidth
-    );
-    if (client == NULL) {
-        std::cout << "An error occurred while trying to create an ENet client host." << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    ENetAddress address = {0};
-    ENetEvent event;
-    ENetPeer* peer = {0};
-    // Bind server address
-    enet_address_set_host(&address, "127.0.0.1");
-    address.port = 7777;
-    // Initiate the connection, allocating the two channels 0 and 1.
-    peer = enet_host_connect(client, &address, 2, 0);
-    if (peer == NULL) {
-        std::cout << "No available peers for initiating an ENet connection." << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    if (enet_host_service(client, &event, 5000) > 0 &&
-        event.type == ENET_EVENT_TYPE_CONNECT) {
-        std::cout << "Connection to Troop of Olde server succeeded." << std::endl;
-    } else {
-        enet_peer_reset(peer);
-        std::cout << "Connection to Troop of Olde server failed." << std::endl;
-    }
-
-    // Initialize ECS world and systems
     flecs::world world;
+    Network network(world);
+    network.connect();
 
     // TEMPORARY: Initialize player character
     // TODO: Character should be created by server
     Position posComp{{0.0f, 0.0f, 0.0f}};
-    auto player_e = world.entity("character");
+    auto player_e = world.entity("LocalPlayer");
     player_e.add<Position>();
     player_e.set<Position>(posComp);
     player_e.add<MovementInput>();
@@ -103,7 +67,7 @@ int main(void)
     register_movement_reconcile_system(world, input_buffer);
     register_movement_input_system(world, input_handler, input_buffer);
     register_movement_system(world);
-    register_movement_networking_system(world, peer, input_buffer, movement_tick);
+    register_movement_networking_system(world, network.peer, input_buffer, movement_tick);
     register_movement_tick_system(world, movement_tick);
     auto render_sys = register_render_system(world, camera);
 
@@ -111,39 +75,7 @@ int main(void)
     while (!WindowShouldClose())
     {
         float dt = GetFrameTime();
-        while (enet_host_service(client, &event, 0) > 0) {
-            switch (event.type) {
-                case ENET_EVENT_TYPE_CONNECT: {
-                    break;
-                }
-                case ENET_EVENT_TYPE_RECEIVE: {
-                    ServerMovementUpdate move_update;
-                    Buffer buffer;
-                    auto state = bitsery::quickDeserialization(
-                        InputAdapter{
-                            event.packet->data,
-                            event.packet->dataLength
-                        },
-                        move_update
-                    );
-                    player_e.set<ServerMovementUpdate>(move_update);
-                    std::cout << "Received position "
-                        << vector3_to_string(move_update.pos)
-                        << " from server for tick "
-                        << (int) move_update.ack_tick
-                        << std::endl;
-                }
-                case ENET_EVENT_TYPE_DISCONNECT: {
-                    break;
-                }
-                case ENET_EVENT_TYPE_DISCONNECT_TIMEOUT: {
-                    break;
-                }
-                case ENET_EVENT_TYPE_NONE: {
-                    break;
-                }
-            }
-        }
+        network.process_events();
         // Progress all ECS timers
         world.progress(dt);
         // Do rendering
@@ -165,7 +97,7 @@ int main(void)
             render_sys.run();
         EndDrawing();
         // Send all messages to server
-        enet_host_flush(client);
+        enet_host_flush(network.client);
     }
     CloseWindow();
 
---
+++ src/client/network.hpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+#include "client/systems/movement_systems.hpp"
+#include "enet.h"
+#include "shared/components/movement.hpp"
+#include "shared/util.hpp"
+
+
+class Network {
+  public:
+    ENetHost* client = {0};
+    ENetPeer* peer = {0};
+    ENetAddress address = {0};
+    ENetEvent event;
+    flecs::world world;
+
+    Network(flecs::world& w) : world(w) {};
+
+    void connect() {
+        connect("127.0.0.1", 7777);
+    }
+
+    void connect(std::string host_addr, int host_port) {
+        client = enet_host_create(
+            NULL, // Create a client host
+            1, // Only allow 1 outgoing connection
+            2, // Allow up to 2 channels to be used, 0 and 1
+            0, // Assume any amount of incoming bandwidth
+            0 // Assume any amount of outgoing bandwidth
+        );
+        if (client == NULL) {
+            std::cout << "An error occurred while trying to create an ENet client host." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        // Bind server address
+        enet_address_set_host(&address,host_addr.c_str());
+        address.port = host_port;
+        // Initiate the connection, allocating the two channels 0 and 1.
+        peer = enet_host_connect(client, &address, 2, 0);
+        if (peer == NULL) {
+            std::cout << "No available peers for initiating an ENet connection." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        if (enet_host_service(client, &event, 5000) > 0 &&
+            event.type == ENET_EVENT_TYPE_CONNECT) {
+            std::cout << "Connection to Troop of Olde server succeeded." << std::endl;
+        } else {
+            enet_peer_reset(peer);
+            std::cout << "Connection to Troop of Olde server failed." << std::endl;
+        }
+    }
+
+    void process_events() {
+        while (enet_host_service(client, &event, 0) > 0) {
+            switch (event.type) {
+                case ENET_EVENT_TYPE_CONNECT: {
+                    break;
+                }
+                case ENET_EVENT_TYPE_RECEIVE: {
+                    process_recv_event();
+                    break;
+                }
+                case ENET_EVENT_TYPE_DISCONNECT: {
+                    break;
+                }
+                case ENET_EVENT_TYPE_DISCONNECT_TIMEOUT: {
+                    break;
+                }
+                case ENET_EVENT_TYPE_NONE: {
+                    break;
+                }
+            }
+        }
+    }
+
+    void process_recv_event() {
+        flecs::entity player_e = world.lookup("LocalPlayer");
+        ServerMovementUpdate move_update = player_e.get_mut<ServerMovementUpdate>();
+        Buffer buffer;
+        auto state = bitsery::quickDeserialization(
+            InputAdapter{
+                event.packet->data,
+                event.packet->dataLength
+            },
+            move_update
+        );
+        std::cout << "Received position "
+            << vector3_to_string(move_update.pos)
+            << " from server for tick "
+            << (int) move_update.ack_tick
+            << std::endl;
+    }
+};
