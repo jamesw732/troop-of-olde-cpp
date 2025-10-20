935989d8a75cc2e6dffcc5cf8b788a92133ca6dd
jamesw732 <jamesmw732@gmail.com>
2025-10-19 21:59:28 -0400

    split network files into static libraries

---
+++ src/server/network.cpp
@@ -0,0 +1,126 @@
+#include <deque>
+#include <iostream>
+#include <unordered_map>
+
+#define ENET_IMPLEMENTATION
+#include "enet.h"
+#include "flecs.h"
+
+#include "server/network.hpp"
+#include "shared/network_components.hpp"
+#include "shared/const.hpp"
+#include "shared/util.hpp"
+
+
+Network::Network(flecs::world& w) : world(w) {};
+
+bool Network::create() {
+    // Create ENet server
+    if (enet_initialize() != 0) {
+        std::cout << "An error occurred while initializing ENET." << std::endl;
+        return 1;
+    }
+    address.host = ENET_HOST_ANY;
+    address.port = 7777;
+    server = enet_host_create(&address, MAX_CLIENTS, 2, 0, 0);
+
+    if (server == NULL) {
+        std::cout << "An error occurred while trying to create an ENet server host." << std::endl;
+        return 1;
+    }
+    std::cout << "Started a server..." << std::endl;
+    ENetEvent event;
+    return 0;
+}
+
+void Network::process_events() {
+    // Process networking events
+    while (enet_host_service(server, &event, 0) > 0) {
+        switch (event.type) {
+            case ENET_EVENT_TYPE_CONNECT: {
+                std::cout << "A new client connected from "
+                    <<  event.peer->address.host.__in6_u.__u6_addr8
+                    << ":"
+                    << (int) event.peer->address.port
+                    << "." << std::endl;
+                auto entity = world.entity();
+                entity.set<NetworkId>({network_id_counter});
+                netid_to_peer[{network_id_counter}] = event.peer;
+                network_id_counter++;
+                event.peer->data = (void*) entity.raw_id();
+                break;
+            }
+
+            case ENET_EVENT_TYPE_RECEIVE: {
+                // handle_packet();
+                auto id = cast_raw_id(event.peer->data);
+                flecs::entity entity(world, id);
+                uint8_t* buffer = event.packet->data;
+                size_t size = event.packet->dataLength;
+                std::vector<uint8_t> packet_data(buffer, buffer + size);
+                RecvPacket recv_packet{entity, packet_data};
+                packets.push_back(recv_packet);
+                break;
+            }
+
+            case ENET_EVENT_TYPE_DISCONNECT: {
+                std::cout << "Client with Entity id "
+                    << cast_raw_id(event.peer->data)
+                    << " disconnected."
+                    << std::endl;
+                auto id = cast_raw_id(event.peer->data);
+                flecs::entity entity(world, id);
+                entity.add<Disconnected>();
+                event.peer->data = NULL;
+                enet_peer_reset(event.peer);
+                break;
+            }
+
+            case ENET_EVENT_TYPE_DISCONNECT_TIMEOUT: {
+                std::cout << "Client with Entity id "
+                    << cast_raw_id(event.peer->data)
+                    << " disconnected due to timeout."
+                    << std::endl;
+                auto id = cast_raw_id(event.peer->data);
+                flecs::entity entity(world, id);
+                entity.add<Disconnected>();
+                event.peer->data = NULL;
+                enet_peer_reset(event.peer);
+                break;
+            }
+
+            case ENET_EVENT_TYPE_NONE:
+                break;
+        }
+    }
+}
+
+void Network::queue_data_unreliable(const NetworkId& network_id, const Buffer& buffer, const size_t size){
+    auto netid_peer = netid_to_peer.find(network_id);
+    if (netid_peer == netid_to_peer.end()) {
+        dbg("Failed to find peer");
+        return;
+    }
+    ENetPeer* peer = netid_peer->second;
+    ENetPacket* packet = enet_packet_create(buffer.data(), size, 0);
+    enet_peer_send(peer, 0, packet);
+}
+
+void Network::queue_data_reliable(const NetworkId& network_id, const Buffer& buffer, const size_t size){
+    auto netid_peer = netid_to_peer.find(network_id);
+    if (netid_peer == netid_to_peer.end()) {
+        dbg("Failed to find peer");
+        return;
+    }
+    ENetPeer* peer = netid_peer->second;
+    ENetPacket* packet = enet_packet_create(buffer.data(), size, ENET_PACKET_FLAG_RELIABLE);
+    enet_peer_send(peer, 1, packet);
+}
+
+void Network::send_network_buffer() {
+    enet_host_flush(server);
+}
+
+ecs_entity_t Network::cast_raw_id(void* raw_id) {
+    return (ecs_entity_t)(uintptr_t) raw_id;
+}
