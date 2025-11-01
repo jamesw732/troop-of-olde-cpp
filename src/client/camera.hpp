72422289abaf3573e061aa2f1a8b236570f785ff
jamesw732 <jamesmw732@gmail.com>
2025-11-01 00:22:22 -0400

    make camera track player

---
+++ src/client/camera.hpp
@@ -0,0 +1,16 @@
+#include <cmath>
+
+#include "flecs.h"
+#include "raylib-cpp.hpp"
+
+#include "components.hpp"
+
+inline void update_camera(flecs::world& world, raylib::Camera3D& camera) {
+    auto local_player = world.lookup("LocalPlayer");
+    if (!local_player.has<RenderPosition>()) {
+        return;
+    }
+    camera.target = local_player.get<RenderPosition>().val;
+    camera.position = local_player.get<RenderPosition>().val
+        + raylib::Vector3{0, 10, 10};
+}
